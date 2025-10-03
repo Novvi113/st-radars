2f6ac64108b29f7d04b20312805998949db7282a
Emmanuel Odujebe <emmajebe@gmail.com>
2025-10-03 15:40:17 +0100

    Updated data for the 25/26 season + New Leagues
    Added the following:
    - Data from the 25/26 season (As of 03/10/25)
    - New Leagues (25/26 only): Second Tiers of Top 5 Leagues + Eredivisie + Liga Portugal

--- radars.py
+++ radars.py
@@ -10,13 +10,13 @@ import io
 #Loading Data
 @st.cache_data
 def get_player_df():
-    return pd.read_csv("Top5PlayerData202026.csv")
+    return pd.read_csv("Top5PlayerData202526.csv")
 
 @st.cache_data
 def get_team_df():
-    return pd.read_csv("Top5TeamData202026.csv")
+    return pd.read_csv("Top5TeamData202526.csv")
 
-player_df =get_player_df()
+player_df = get_player_df()
 team_df = get_team_df()
 
 #Data cleaning and transformation
@@ -33,7 +33,7 @@ def poss_adj(metric, poss = df["Poss"]):
     return metric*(50/poss)
 
 #Filtering out players without required minutes
-df_clean = df[df["Min_Playing"] >= 450]
+df_clean = df[df["Min_Playing"] >= 100]
 
 vars_to_90 = ["Touches_Touches", "Def Pen_Touches", "Def 3rd_Touches", "Mid 3rd_Touches", "Att 3rd_Touches", "Att Pen_Touches", "Live_Touches", "Att_Take", "Succ_Take", "Tkld_Take", "Carries_Carries", "TotDist_Carries", "PrgDist_Carries", "PrgC_Carries", "Final_Third_Carries", "CPA_Carries", "Mis_Carries", "Dis_Carries", "Rec_Receiving", "PrgR_Receiving", "Gls_Standard", "FK_Standard", "PK_Standard", "xG_Expected", "npxG_Expected", "G_minus_xG_Expected", "np:G_minus_xG_Expected", "Att", "Live_Pass", "Dead_Pass", "FK_Pass", "TB_Pass", "Sw_Pass", "Crs_Pass", "Off_Outcomes", "Blocks_Outcomes", "Cmp_Total", "Att_Total", "TotDist_Total", "PrgDist_Total", "Cmp_Short", "Att_Short", "Cmp_Medium", "Att_Medium", "Cmp_Long", "Att_Long", "Ast", "xAG", "xA_Expected", "A_minus_xAG_Expected", "KP", "Final_Third", "PPA", "CrsPA", "PrgP", "PassLive_SCA", "PassDead_SCA", "TO_SCA", "Sh_SCA", "Fld_SCA", "Def_SCA", "Fls", "Fld", "Off", "Crs", "TklW", "PKwon", "PKcon", "OG", "Recov", "Won_Aerial", "Lost_Aerial", "Def 3rd_Tackles", "Mid 3rd_Tackles", "Att 3rd_Tackles", "Tkl_Challenges", "Att_Challenges", "Lost_Challenges", "Blocks_Blocks", "Sh_Blocks", "Pass_Blocks", "Int", "Tkl+Int", "Clr", "Err"]
 
